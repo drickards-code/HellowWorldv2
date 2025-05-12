35f4b0164a5a57c1920567a63f2ac85018166e70
drickards-code <Daniel.rickards@telefonicatech.uk>
2025-05-12 16:02:59 +0100

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,7 +7,7 @@ export const About = (props) => {
         <div className="row">
           <div className="col-xs-12 col-md-6">
             {" "}
-            <img src="img/ProfilePicGreySweatshirtBayPicCropped.jpg" className="img-responsive" alt="" />{" "}
+            <img src="img/helloworld.png" className="img-responsive" alt="" />{" "}
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
